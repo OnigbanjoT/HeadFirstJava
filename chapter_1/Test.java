53926eb755aaf10c7b8ff1a3c70d779c7de93b97
Toludono <onigbanjo.toluwalase@gmail.com>
2022-03-22 11:17:37 -0400

    first commit

---
+++ chapter_1/Test.java
@@ -0,0 +1,13 @@
+class Test {
+  public static void main (String[] args) {
+    int x = 0;
+
+    while () {
+      System.out.print("a");
+      if (x < 1) {
+        System.out.print(" ");
+      }
+      
+    }
+  }
+}
